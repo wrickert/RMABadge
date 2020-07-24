99dbabb4055805002714ff212e2c6a506e7d2a17
William Rickert <brickert7@gmail.com>
2020-07-24 11:39:20 -0500

    Wrote some test code'

--- Badges/BlueTeamCon/Firmware/BlueTeamCon/Src/main.c
+++ Badges/BlueTeamCon/Firmware/BlueTeamCon/Src/main.c
@@ -99,6 +99,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+      HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_12);
+      HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
